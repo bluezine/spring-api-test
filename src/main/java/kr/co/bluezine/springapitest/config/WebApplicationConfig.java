77abd7e165cdb2fcfed59822d701d1622401a53d
Kisig Ian Seo <bluezine0930@gmail.com>
2021-08-21 19:43:59 +0900

    message 및 validate 메시지 사용하도록 선언

--- src/main/java/kr/co/bluezine/springapitest/config/WebApplicationConfig.java
+++ src/main/java/kr/co/bluezine/springapitest/config/WebApplicationConfig.java
@@ -2,8 +2,11 @@ package kr.co.bluezine.springapitest.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
@@ -32,5 +35,28 @@ public class WebApplicationConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.setConnectTimeout(Duration.ofMillis(connectTimeout)).setReadTimeout(Duration.ofMillis(readTimeout)).build();
+    }
+
+    /*
+     * 메세지 공통사용을 위한 Bean
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    /*
+     * View 단 Valid 메시지를 위한 Bean
+     */
+    @Bean
+    public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
     }
 }
