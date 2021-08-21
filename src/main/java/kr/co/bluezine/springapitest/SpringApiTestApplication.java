91f863c7fa12337f4be0f7018b1e1fd3a7d9d51b
Kisig Ian Seo <bluezine0930@gmail.com>
2021-08-21 16:52:34 +0900

    Spring Boot 프로젝트 초기화

---
+++ src/main/java/kr/co/bluezine/springapitest/SpringApiTestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bluezine.springapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiTestApplication.class, args);
+    }
+
+}
